ba90ee6f1cc6de564a541ab848436d69173e0752
Van Nguyen <van@shirotech.com>
2019-07-17 18:11:35 +0700

    Don't show on input after changing

--- pikaday.js
+++ pikaday.js
@@ -581,9 +581,6 @@
             if (isDate(date)) {
               self.setDate(date);
             }
-            if (!self._v) {
-                self.show();
-            }
         };
 
         self._onInputFocus = function()
